9d58279d0980544f082b43c950e1d984edc4f17a
Jeevanantham Palanisamy <jeevanantham143.cse@gmail.com>
2019-05-04 19:03:52 +0530

    Test

---
+++ app/src/main/java/com/jeeva/fuellevelpredictor/calendarpoc/CalendarActivity.java
@@ -0,0 +1,13 @@
+package com.jeeva.fuellevelpredictor.calendarpoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
